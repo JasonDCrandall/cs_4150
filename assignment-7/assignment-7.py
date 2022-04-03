be21a810b00654047b59d2f20e789bfe04acd39a
JasonDCrandall <jcrandall7@gmail.com>
2022-04-03 07:54:05 -0600

    mostly working

--- assignment-7/assignment-7.py
+++ assignment-7/assignment-7.py
@@ -75,7 +75,9 @@ class Edge:
         return other.__lt__(self)
 
     def __eq__(self, other):
-        return self.weight == other.weight and self.from_v == other.from_v and self.to_v == other.to_v
+        if other != None:
+            return self.weight == other.weight and self.from_v == other.from_v and self.to_v == other.to_v
+        return False
 
     def printEdge(self):
         print("{} {}".format(self.from_v, self.to_v))
@@ -98,14 +100,13 @@ def jarnikAlgorithm(weights:list, start_vertex:int):
             for v in range(len(weights[edge.from_v])):
                 if weights[edge.from_v][v] != 0:
                     priorityQueue.append(Edge(edge.from_v, v, weights[edge.from_v][v]))
-            priorityQueue.sort()
         if edge.to_v in unmarked:
             edges.append(edge)
             unmarked.remove(edge.to_v)
             for v in range(len(weights[edge.to_v])):
                 if weights[edge.to_v][v] != 0:
                     priorityQueue.append(Edge(edge.to_v, v, weights[edge.to_v][v]))
-            priorityQueue.sort()
+        priorityQueue.sort()
     return edges
 
 def kruskalAlgorithm(weights:list):
@@ -128,9 +129,65 @@ def kruskalAlgorithm(weights:list):
                 vertexMap[v] = updatedComponent
     return edges
 
+marked = []
 def boruvkaAlgorithm(weights:list):
+    global marked
+    marked = [False]*len(weights)
+    for v in range(len(weights)):
+        marked[v] = [False]*len(weights)
     edges = []
+    component = [i for i in range(len(weights))]
+    count = len(weights)
+    while count > 1:
+        addAllSafeEdges(weights, edges, count, component)
+        count = countAndLabel(weights, edges, component)
     return edges
+
+def countAndLabel(weights:list, edges:list, component:list):
+    count = 0
+    unmarked = [v for v in range(len(weights))]
+    for v in range(len(weights)):
+        if v in unmarked:
+            count += 1
+            labelOne(weights, edges, v, count, unmarked, component)
+    return count
+
+def labelOne(weights:list, edges:list, v:int, count:int, unmarked:list, component:list):
+    bag = []
+    bag.append(v)
+    while bag:
+        v = bag.pop(0)
+        if v in unmarked:
+            unmarked.remove(v)
+            component[v] = count
+            for w in range(len(weights[v])):
+                if weights[v][w] != 0:
+                    if Edge(v, w, weights[v][w]) not in edges:
+                        bag.append(w)
+
+
+def addAllSafeEdges(weights:list, edges:list, count:int, component:list):
+    print('adding safe edges')
+    safe = [None for _ in range(count)]
+    allEdges = []
+    for i in range(len(weights)):
+        for j in range(len(weights[i])):
+            if weights[i][j] != 0:
+                allEdges.append(Edge(i, j, weights[i][j]))
+    for edge in allEdges:
+        u = edge.from_v
+        v = edge.to_v
+        if component[u] != component[v]:
+            if safe[component[u]] == None or edge.__lt__(safe[component[u]]):
+                safe[component[u]] = edge
+            if safe[component[v]] == None or edge.__lt__(safe[component[v]]):
+                safe[component[v]] = edge
+    for i in range(count):
+        if not marked[safe[i].from_v][safe[i].to_v]:
+            edges.append(safe[i])
+            marked[safe[i].from_v][safe[i].to_v] = True
+            marked[safe[i].to_v][safe[i].from_v] = True
+
 
 
 def printEdges(edges:list):
